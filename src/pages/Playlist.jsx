12b58ae2543672e68b842fa29fdca2d76605960a
macintoshi <dekodumbadze@gmail.com>
2025-05-27 17:24:33 +0400

    fixed playlist code , reset scss commented.

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,25 +1,22 @@
 import classes from '../modules/Playlist.module.scss'
 import playlistImg from '../assets/playlist-img.png'
+
 const Playlist = () => {
     return (
         // <Header/>
         <div className={classes['playlist']}>
             <div className={classes['main-wrapper']}>
-
                 <div className={classes['title']}>
                     <b>Your Playlists</b>
                     <p>Your personal collection of curated music</p>
                 </div>
-
                 <div className={classes['main-content']}>
-
                     <div className={classes['single-playlist']}>
 
                         <div className={classes['single-image']}>
                             <img src={playlistImg} alt='playlist'/>
                             <p>img</p>
                         </div>
-
                         <div className={classes['single-info']}>
                             <h1>playlist name</h1>
                             <p>created with friendsName</p>
@@ -27,17 +24,13 @@ const Playlist = () => {
                                 <button>Play</button>
                                 <button>edit</button>
                             </div>
-
                         </div>
                     </div>
-
                     <div className={classes['single-playlist']}>
-
                         <div className={classes['single-image']}>
                             <img src={playlistImg} alt='playlist'/>
                             <p>img</p>
                         </div>
-
                         <div className={classes['single-info']}>
                             <h1>playlist name</h1>
                             <p>created with friendsName</p>
@@ -45,17 +38,14 @@ const Playlist = () => {
                                 <button>Play</button>
                                 <button>edit</button>
                             </div>
-
                         </div>
                     </div>
-
                     <div className={classes['single-playlist']}>
 
                         <div className={classes['single-image']}>
                             <img src={playlistImg} alt='playlist'/>
                             <p>img</p>
                         </div>
-
                         <div className={classes['single-info']}>
                             <h1>playlist name</h1>
                             <p>created with friendsName</p>
@@ -63,7 +53,6 @@ const Playlist = () => {
                                 <button>Play</button>
                                 <button>edit</button>
                             </div>
-
                         </div>
                     </div>
 
