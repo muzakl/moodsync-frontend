03cadb91f60866533046572083a0c3925e5e13d6
macintoshi <dekodumbadze@gmail.com>
2025-06-02 16:31:00 +0400

    fixed lil header added navigate , and fixed lil scss. lil changes in router.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import {BrowserRouter, Route, Routes} from "react-router";
 import Playlist from "./pages/Playlist.jsx";
 import Error from './pages/Error.jsx'
 import PlaylistInfo from "./components/playlist-info.jsx";
+import CreatePlaylistSection from "./pages/CreatePlaylistSection/index.jsx";
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App/>}/>
+            <Route path="/" element={<CreatePlaylistSection/>}/>
             <Route path="/playlist" element={<Playlist/>}/>
             <Route path="/playlist-info" element={<PlaylistInfo/>}/>
-
             <Route path="/*" element={<Error/>} />
         </Routes>
     </BrowserRouter>
