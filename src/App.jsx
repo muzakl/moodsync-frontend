979cc325a99f1d4771a4bfa3f3592ec5b6707ad0
ssbombaguy <giorgimaisuradze2008@gmail.com>
2025-06-03 20:35:45 +0400

    add login and registration pages with social media icons

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import CreatePlaylistSection from "./pages/CreatePlaylistSection/index.jsx";
-
-const App = () => {
-    return (
-        <CreatePlaylistSection />
-    )
-}
-
-export default App
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,48 +1,90 @@
 import classes from '../modules/Register.module.scss';
 import line from "../assets/line.svg";
-import github from "../assets/github.svg";
-import google from "../assets/google.svg";
-import facebook from "../assets/facebook.svg";
 import spotify from "../assets/spotify.svg";
+import {useState} from "react";
 
 export const Register = () => {
+    const [step,setStep] = useState(0);
+    const continueHandler = () => {
+        setStep(step+1);
+    }
     return (
         <>
-            <div className={classes["background"]}>
-                <div className={classes["container"]}>
-                    <div className={classes["main"]}>
-                        <div className={classes["stepper"]}>
-                            <h1>1</h1>
-                            <h1>2</h1>
-                            <h1>3</h1>
-                        </div>
-                        <h1 className={classes["title"]}>Create your account</h1>
-                        <div className={classes["form"]}>
-                            <input type="text" placeholder="Fullname" className={classes["input"]}/>
-                            <input type="text" placeholder="Username" className={classes["input"]}/>
-                            <input type="password" placeholder="Password" className={classes["input"]}/>
-                            <input type="password" placeholder="Confirm password" className={classes["input"]}/>
-                            <button type="submit" className={classes["continue-btn"]}>Continue</button>
-                        </div>
-                    </div>
-                    <div className={classes["extra"]}>
-                        <div className={classes["extra-text"]}>
-                            <img src={line} alt="line" className={classes["line"]}/>
-                            <p className={classes["text"]}>or</p>
-                            <img src={line} alt="line" className={classes["line"]}/>
-                        </div>
-                        <div className={classes["socials"]}>
-                            <div>
-                                <button className={classes["continue-spt"]}><img src={spotify} alt={"spotify"}/>Continue
-                                    with Spotify
-                                </button>
+            {step === 0 ? (
+                <div className={classes["background"]}>
+                    <div className={classes["container"]}>
+                        <div className={classes["main"]}>
+                            <div className={classes["stepper"]}>
+                                <h1>1</h1>
+                                <h1>2</h1>
+                                <h1>3</h1>
+                            </div>
+                            <h1 className={classes["title"]}>Create your account</h1>
+                            <div className={classes["form"]}>
+                                <input type="text" placeholder="Fullname" className={classes["input"]}/>
+                                <input type="text" placeholder="Username" className={classes["input"]}/>
+                                <input type="password" placeholder="Password" className={classes["input"]}/>
+                                <input type="password" placeholder="Confirm password" className={classes["input"]}/>
+                                <button type="submit" className={classes["continue-btn"]} onClick={continueHandler}>Continue</button>
                             </div>
                         </div>
+                        <div className={classes["extra"]}>
+                            <div className={classes["extra-text"]}>
+                                <img src={line} alt="line" className={classes["line"]}/>
+                                <p className={classes["text"]}>or</p>
+                                <img src={line} alt="line" className={classes["line"]}/>
+                            </div>
+                            <div className={classes["socials"]}>
+                                <div>
+                                    <button className={classes["continue-spt"]}><img src={spotify} alt={"spotify"}/>Continue
+                                        with Spotify
+                                    </button>
+                                </div>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-
+            ) : step === 1 ? (
+                <div className={classes["background"]}>
+                    <div className={classes["container"]}>
+                        <div className={classes["main"]}>
+                            <div className={classes["stepper"]}>
+                                <h1>1</h1>
+                                <h1>2</h1>
+                                <h1>3</h1>
+                            </div>
+                            <h1 className={classes["title"]}>Add your address</h1>
+                            <div className={classes["form"]}>
+                                <input type="text" placeholder="Street Address" className={classes["input"]}/>
+                                <input type="text" placeholder="City" className={classes["input"]}/>
+                                <input type="text" placeholder="State/Province" className={classes["input"]}/>
+                                <input type="text" placeholder="Postal Code" className={classes["input"]}/>
+                                <button type="submit" className={classes["continue-btn"]} onClick={continueHandler}>Continue</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : step ===2 && (
+                <div className={classes["background"]}>
+                    <div className={classes["container"]}>
+                        <div className={classes["main"]}>
+                            <div className={classes["stepper"]}>
+                                <h1>1</h1>
+                                <h1>2</h1>
+                                <h1>3</h1>
+                            </div>
+                            <h1 className={classes["title"]}>Complete your profile</h1>
+                            <div className={classes["form"]}>
+                                <input type="text" placeholder="Profile Picture URL" className={classes["input"]}/>
+                                <input type="text" placeholder="Bio" className={classes["input"]}/>
+                                <button type="submit" className={classes["continue-btn"]}>Finish Registration</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+            }
         </>
     )
 }
